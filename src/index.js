636824ae3549674eb7d85cfbbc6255052b9feaa8
egor-kuch <egor-kuch@yandex-team.ru>
2023-05-18 09:45:31 +0300

    Манифест, общие стили, медиа-миксин, хэдер

--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './index.scss';
+import App from './components/App/App.js';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 import state, {subscribe, updateCheckbox} from "./Redux/state";
 import Registration from "./components/Registration/Registration";
 
-let rerenderAllTree =  () => {
-    const root = ReactDOM.createRoot(document.getElementById('root'));
-    root.render(
-        <React.StrictMode>
-            <BrowserRouter>
-                <Routes>
-                    <Route path='/registration' element={<Registration/>}/>
-                    <Route path='*' element={<App state={state} updateCheckbox={updateCheckbox}/>}/>
-                </Routes>
-            </BrowserRouter>
-        </React.StrictMode>
-    );
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path='*' element={<App state={state}/>}/>
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>
+);
 
     reportWebVitals();
 }
